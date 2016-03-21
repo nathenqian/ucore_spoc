1a4f9cf8d7f11e110ec82cd1e817d127067562ab
高思达 <gsd13@mails.tsinghua.edu.cn>
2016-03-21 09:26:11 +0800

    增加了重要部分的注释

--- spoc_9/main.py
+++ spoc_9/main.py
@@ -1,7 +1,7 @@
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option('-t', '--delte_time', default='3', help='delta time',  action='store', type='int', dest='delta_t')
+parser.add_option('-t', '--delta_time', default='3', help='delta time',  action='store', type='int', dest='delta_t')
 
 (options, args) = parser.parse_args()
 delta_t = options.delta_t
@@ -10,6 +10,10 @@ test_list = [0, 1, 2, 1, 0, 2, 3, 1, 3]
 # delta_t = 3
 memory_page = {}
 total_miss = 0
+
+# 设计思路：
+# 我们每访问一页（即该页进入窗口），就对它的“访问次数”加1
+# 窗口移动出去时，把它的“访问次数”减1，则小于等于0的是无效的页
 
 def remove_page(memory, index):
     memory[index] -= 1
@@ -25,7 +29,9 @@ def add_page(memory, index):
     memory[index] += 1
 
 for t in range(0, len(test_list)):
+    # 找到刚刚失效的位置
     last_index = t - delta_t
+    # 如果该位置存在，删掉它
     if last_index >= 0:
         remove_page(memory_page, test_list[last_index])
     if has_page(memory_page, test_list[t]):
